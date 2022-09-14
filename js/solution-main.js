b90fb6db1a86887e57a75ce0289d4c7d5d0fdd8b
ohy9205 <ohy9205@naver.com>
2022-09-14 12:16:29 +0900

    [solution] 게임 시작, 정지 구현

--- js/solution-main.js
+++ js/solution-main.js
@@ -1,15 +1,91 @@
 'use strict';
 
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 10;
+const BUG_COUNT = 7;
+const GAME_DURATION_SEC = 5;
+
 const gameField = document.querySelector('.game-field');
 // 이미지 랜덤 배치를 위해 filed의  전체적인 사이즈와 위치 알아내기
 const fieldRect = gameField.getBoundingClientRect();
+const gameBtn = document.querySelector('.game-button');
+const gameTimer = document.querySelector('.game-timer');
+const gameScore = document.querySelector('.game-score');
+const gamePopUp = document.querySelector('.pop-up');
+const gamePopUpMessage = document.querySelector('.pop-up-message');
+const gamePopUpRefresh = document.querySelector('.pop-up-refresh');
 
-/**벌레, 당근 생성 */
+// 게임의 상태를 기억하는 변수들
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', ()=>{
+  if(started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started; // 게임 상태 변경
+});
+
+/**게임 시작 함수 */
+function startGame() {
+  initGame(); // 벌레,당근 생성, 당근 갯수 초기화
+  showStopBtn(); // 정지 버튼으로 변경
+  startGameTimer(); // 타이머 시작
+}
+
+/**게임 정지 함수 */
+function stopGame() {
+  stopGameTimer(); // 타이머 정지
+  hideStartBtn();// 상단 버튼 사라짐
+  showPopUpWithText('REPLAY?');// 팝업 등장
+}
+
+/**버튼을 중지버튼으로 변경 */
+function showStopBtn() {
+  const icon = gameBtn.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
+
+/**게임 정지 시 버튼을 숨김 */
+function hideStartBtn() {
+  gameBtn.style.visibility = 'hidden';
+}
+
+/**타이머 시작 */
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC; // 남은 시간동안 setInerval이 실행되게 만든다
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(()=>{
+    if(remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  },1000);
+}
+
+/**타이머 정지 */
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+/**타이머를 분:초 단위로 업데이트한다 */
+function updateTimerText(remainingTimeSec) {
+  const minutes = Math.floor(remainingTimeSec / 60);
+  const seconds = Math.floor(remainingTimeSec % 60);
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+/**벌레, 당근 생성, 당근갯수 카운트 초기화 */
 function initGame() {
-  console.log(fieldRect);
-  addItem('carrot', 10, '../img/carrot.png');
-  addItem('bug', 7, '../img/bug.png');
+  gameField.innerHTML = '';
+  addItem('carrot', CARROT_COUNT, '../img/carrot.png');
+  addItem('bug', BUG_COUNT, '../img/bug.png');
+  gameScore.innerHTML = CARROT_COUNT;
 }
 
 /**게임 필드에 아이템을 추가 */
@@ -39,4 +115,8 @@ function randomNumber(min, max) {
   return Math.random()*(max-min)+min;
 }
 
-initGame();
+/**팝업 창 등장, 초기화 기능 */
+function showPopUpWithText(text) {
+  gamePopUpMessage.innerText = text;
+  gamePopUp.classList.remove('pop-up-hide');
+}
